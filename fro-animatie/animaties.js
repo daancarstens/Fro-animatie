2dffad6d5d4794a6875ad110341c85faa66dfcf3
daancarstens <55231123+daancarstens@users.noreply.github.com>
2020-11-27 13:19:25 +0100

    tot video 11

---
+++ fro-animatie/animaties.js
@@ -0,0 +1,21 @@
+document.querySelector('.animeer-knop').addEventListener('click', (e) => {
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')
+});
+
+setTimeout( () => {
+    document.querySelector('.vlakje').classList.toggle('vlakje__naar-rechts')
+}, 2000);
+
+const menuKnop = document.querySelector('.nav__knop');
+const navigatieItems = document.querySelectorAll('.nav__link');
+
+menuKnop.addEventListener('click', () => {
+    navigatieItems.forEach( (item,index) => {
+        setTimeout( () => {
+            item.classList.toggle('nav__link--schuif-in');
+        }, 100 * index);
+    })
+    document.querySelector('.fa-bars').classList.toggle('verberg');
+    document.querySelector('.fa-times').classList.toggle('verberg');
+
+});
